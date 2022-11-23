4c0e667d6464f774ef17cdac7bb9741d1a4379e3
unknown <alexandre.francony@edu.devinci.fr>
2022-11-23 20:29:05 +0100

    First commit

---
+++ Francony_Morel/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Francony_Morel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choix = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("");
+                Console.WriteLine("1. ");
+                Console.WriteLine("2. ");
+                Console.WriteLine("3. ");
+                Console.WriteLine("4. ");
+                Console.WriteLine("9. Quitter");
+                Console.Write("Votre choix : ");
+                choix = Utils.saisieNombre();
+                switch (choix) 
+                {//Le choix 0 doit Rester vide pour le cas où l'utilisateur ne rentre rien
+                    case 1:
+                    break;
+
+                    case 2://Création
+                    break;
+
+                    case 3://Infos
+                    break;
+
+                    case 4://Read Data
+                    break;
+
+                    case 9://Quitter
+                        Console.Clear();
+                    break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Erreur de saisie, veuillez recommencer");
+                    break;
+                }
+            } while (choix != 9);
+        }
+    }
+}
---
+++ Francony_Morel/Utils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Francony_Morel
+{
+    internal class Utils
+    {
+        public static int saisieNombre()
+        {
+            //Saisie d'un nombre entier non null et retourne le nombre
+            int? nombre = null;
+            while (nombre == null)
+            {
+                try
+                {
+                    nombre = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Le nombre ne peut pas être vide, Veuillez recommencer.");
+                }
+            }
+            return (int)nombre;
+        }
+
+        public static string saisieString()
+        {
+            string? s = "";
+            while (s == "" || s == null)
+            {
+                s = Console.ReadLine();
+                if (s == "" || s == null)
+                {
+                    Console.WriteLine("La saisie ne peut pas être vide, veuillez recommencer.");
+                }
+            }
+            return s;
+        }
+
+        public static void Wait()
+        {
+            Console.WriteLine("Appuyez sur une touche pour continuer...");
+            Console.ReadKey();
+        }
+    }
+}
